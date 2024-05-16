fc488d835bb2172f5292c6362cd667b03b985280
Eduardo Pires <94654833+Eduardo-Pires@users.noreply.github.com>
2024-05-16 19:48:39 -0300

    desafio de projeto concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphoneDeVictor = new Iphone(numero:"4002-8922", modelo:"iphone14", imei:"490154203237518", memoria:16);
+iphoneDeVictor.Ligar();
+iphoneDeVictor.ReceberLigacao();
+iphoneDeVictor.InstalarAplicativo("Imessage");
+
+Console.WriteLine();
+
+Nokia nokiaDeEduardo = new Nokia(numero:"0800-777-7000", modelo:"nokia3310", imei:"352684072891234", memoria:2);
+nokiaDeEduardo.Ligar();
+nokiaDeEduardo.ReceberLigacao();
+nokiaDeEduardo.InstalarAplicativo("Opera Mini");
